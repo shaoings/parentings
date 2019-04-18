1418fee492a9a2f99b59af087b35e6c0ed8e3a35
shaoings <376905553@qq.com>
2019-04-18 12:52:42 +0800

    阿里大于

---
+++ ings_sms_service/src/main/java/com/hhdb/sms/ings_sms_service/IngsSmsServiceApplication.java
@@ -0,0 +1,14 @@
+package com.hhdb.sms.ings_sms_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngsSmsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngsSmsServiceApplication.class, args);
+    }
+
+}
+
---
+++ ings_sms_service/src/test/java/com/hhdb/sms/ings_sms_service/IngsSmsServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hhdb.sms.ings_sms_service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IngsSmsServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
